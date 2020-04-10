15b1a2137b112da26504e3069af6708280a415a7
arvisdev <arvisdev@gmail.com>
2020-04-10 02:57:32 -0500

    Applied Owner Index with Controller.  closes #2.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,6 +1,11 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import java.util.List;
+import java.util.Set;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,10 +16,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController
 {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService)
+    {
+        this.ownerService = ownerService;
+    }
+    
     
     @RequestMapping({"" , "/", "/index", "/index.html"})
-    public String listOwners()
+    public String listOwners(Model model)
     {
+        model.addAttribute("owners", ownerService.findAll());
+        
+        Set<Owner> so = ownerService.findAll();
+        for (Owner o : so)
+        {
+            System.out.println(o.getLastName() + ", " + o.getFirstName());
+        }
+
         return "owners/index";
     }
     
