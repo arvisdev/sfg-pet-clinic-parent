6784722c2c0a51e03901f155bc948392158e4633
arvisdev <arvisdev@gmail.com>
2020-04-12 01:45:33 -0500

    Creatde Vet Speciality Entity, Assoicate to Vet.  closes #17

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ *
+ * @author arvisdev
+ */
+public class Speciality extends BaseEntity
+{
+    private String description;
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -6,6 +6,8 @@
 
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  *
  * @author arvisdev
@@ -14,4 +16,16 @@ public class Vet extends Person
 {
 	private static final long serialVersionUID = 1L;
     
+        private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities()
+    {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities)
+    {
+        this.specialities = specialities;
+    }
+        
 }
