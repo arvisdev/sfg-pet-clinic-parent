3cc384a6d92f2d3c0ef25fe4d1f0ff22401a36e2
arvisdev <arvisdev@gmail.com>
2020-04-14 18:08:11 -0500

    Created Visit Entity.  closes #28.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -6,15 +6,29 @@
 package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Cleanup;
 
 /**
  *
  * @author arvisdev
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity
 {
+    @Column(name = "date")
     private LocalDate date;
+    
+    @Column(name = "description")
     private String description;
+    
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet Pet;
 
     public LocalDate getDate()
