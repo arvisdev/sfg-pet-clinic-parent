7ff43ae4e4e40cdf7b96863f67e1fe992ef755c6
arvisdev <arvisdev@gmail.com>
2020-04-14 18:33:56 -0500

    Added Spring Data JPA Repositories.  closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VisitRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author arvisdev
+ */
+public interface VisitRepository extends CrudRepository<Visit, Long>
+{
+    
+}
