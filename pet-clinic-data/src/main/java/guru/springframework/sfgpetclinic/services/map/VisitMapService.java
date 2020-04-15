b53e69837e9d1c9d319f48097316d921b72a5acd
arvisdev <arvisdev@gmail.com>
2020-04-14 21:29:48 -0500

    Created Visit Map Service.  closes #39.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,60 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import java.util.Set;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService
+{
+    
+    private final VisitService visitService;
+
+    public VisitMapService(VisitService visitService)
+    {
+        this.visitService = visitService;
+    }
+
+    @Override
+    public Set<Visit> findAll()
+    {
+        // This is an override of the abstract class
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id)
+    {
+        // This is an override of the abstract class
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit)
+    {
+        if(visit.getPet()== null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+            || visit.getPet().getOwner().getId() == null)
+        {
+            throw new RuntimeException("Invalid Visit");
+        }
+        
+        // This is an override of the interface
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object)
+    {
+        // This is an override of the abstract class
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id)
+    {
+        // This is an override of the abstract class
+        super.deleteById(id);
+    }
+
+}
